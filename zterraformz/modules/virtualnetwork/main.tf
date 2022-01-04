64d34094729f85af5048e44372056723c37b64d2
vnyaryan <49775576+vnyaryan@users.noreply.github.com>
2022-01-04 19:08:03 +0530

    Update main.tf

--- zterraformz/modules/virtualnetwork/main.tf
+++ zterraformz/modules/virtualnetwork/main.tf
@@ -3,5 +3,6 @@ resource "azurerm_virtual_network" "vnet" {
   address_space       = var.vnet_address_space
   location            = var.vnet_location
   resource_group_name = var.vnet_resource_group_name
+  enforce_private_link_endpoint_network_policies = var.enforce_private_link_endpoint_network_policies
 }
 
